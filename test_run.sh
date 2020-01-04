346efda7e7e3a14a549695f960b7a2f22a9e2518
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-01-04 15:56:49 +0800

    test_run.sh

---
+++ test_run.sh
@@ -0,0 +1,12 @@
+
+
+# python setup.py build develop
+
+# python tools/train_net.py \
+#   --num-gpus 8 \
+# 	--config-file configs/COCO-Detection/faster_rcnn_R_50_C4_1x.yaml
+
+python3 tools/train_net.py \
+  --num-gpus 6 \
+  --config-file configs/COCO-Detection/faster_rcnn_R_50_C4_1x.yaml \
+  SOLVER.IMS_PER_BATCH 12 SOLVER.BASE_LR 0.015
