164fee5461e14144f3ac5e62cc65ce274f72fc6b
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-01-07 09:22:06 +0800

    register denseresnet

--- tools/train_net.py
+++ tools/train_net.py
@@ -39,6 +39,7 @@ from detectron2.modeling import GeneralizedRCNNWithTTA
 
 from detectron2_exp.TOOLS.modelarts_hook import ModelArtsHook
 from template_lib.utils.config import setup_logger_and_redirect_stdout
+from detectron2_exp.TOOLS import dense_resnet
 
 
 class Trainer(DefaultTrainer):
