b786b39989bb209d3ae64fd54905036642cd53f3
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2020-02-17 18:21:49 +0800

    entropy rpn

--- tools/train_net.py
+++ tools/train_net.py
@@ -40,7 +40,7 @@ from detectron2.modeling import GeneralizedRCNNWithTTA
 from template_lib.utils.detection2_utils import D2Utils
 
 from detectron2_exp.TOOLS import modelarts_hook, save2text_hook
-from detectron2_exp.TOOLS import multi_label_roi_head
+from detectron2_exp.TOOLS import multi_label_roi_head, entropy_rpn
 
 
 class Trainer(DefaultTrainer):
